321a037f5bb3349c59ce966f2898c98dbb549feb
imohtesham <ibraahimm92@gmail.com>
2022-11-13 23:30:44 +0530

    Ability to ensure there is no Duplicate Entry of the same Person

---
+++ src/AddressBookMain.java
@@ -0,0 +1,25 @@
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        System.out.println("....WELCOME TO ADDRESS BOOK....");
+        Contacts newContact = new Contacts();
+        newContact.setFirstName("Ibrahim \n");
+        newContact.setLastName("Mohd \n");
+        newContact.setAddress("hi tech  \n");
+        newContact.setCity("Hyderabad \n");
+        newContact.setState("Telangana \n");
+        newContact.setZip(516106);
+        newContact.setEmail("ibrahim83@gmail.com \n");
+        newContact.setPhoneNumber(1234567890);
+        System.out.println("The Contact is : " + newContact);
+
+        // To add new contact
+        AddressBook addressBook = new AddressBook();
+        addressBook.operation();
+        addressBook.add();
+        addressBook.delete();
+        addressBook.edit();
+        addressBook.sortCity();
+
+    }
+}
---
+++ src/Sort.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class Sort {
+    static Comparator<Contacts> compareCity = new Comparator<Contacts>() {
+        public int compare(Contacts one, Contacts two) {
+            return one.getCity().compareTo(two.getCity());
+        }
+    };
+}
